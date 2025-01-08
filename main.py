5b2afafdbe359c597c596865fa9cd7c0c61c72f2
cclally <cclally@hotmail.com>
2025-01-07 21:20:36 -0600

    Finished the assignment. Converted dictionary of characters to a list with a key of char, and the count of each char. sorted the list. Then filtered out non-alphabet characters, and printed to terminal.

--- main.py
+++ main.py
@@ -5,9 +5,24 @@ def main():
     num_chars = get_char_count(text)
     chars_dict = get_chars_dict(text)
 
-    print(num_words)
-    print(num_chars)
-    print(chars_dict)
+    list_of_dicts = [{'char': key, 'num': value} for key, value in chars_dict.items()]
+    list_of_dicts.sort(reverse=True, key=sort_on)
+
+    print("--- Begin report of", book_path, "---")
+    print(num_words, "words found in", book_path)
+    print(num_chars, "total characters found in", book_path, '\n')
+
+    for d in list_of_dicts:
+        if (d["char"].isalpha()):
+            print("The",'\'' + d["char"] + '\'', "character was found", d["num"], "times")
+        else:
+            None
+
+    print("--- End report ---")
+
+def sort_on(dict):
+    return dict["num"]
+
 
 def get_book_text(path):
     with open(path) as f:
